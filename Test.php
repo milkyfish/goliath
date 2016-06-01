13f4a3739d47d41bfe0a8e807d7a61758726c971
milkyfish <milkyfish@daum.net>
2016-06-01 08:52:24 +0700

    Hotfix #2

--- Test.php
+++ Test.php
@@ -1,8 +1,8 @@
 <?php
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function equalPasswords() {
-		$this->assertEquals(1,1);
+	public function testOnePlusOne() {
+		$this->assertEquals(1+1,1);
   	}
 }
 ?>
